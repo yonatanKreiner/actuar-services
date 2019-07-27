596947a3cd39d02cec8da08d6199631cddeb940e
yonatan kreiner <yonatankreiner@gmail.com>
2019-07-27 20:54:48 +0300

    fix path to excel file

--- interest-calculator/src/excel.js
+++ interest-calculator/src/excel.js
@@ -1,7 +1,7 @@
 const xlsx = require('node-xlsx');
 
 const getExcel = () => {
-    const path = './assets/actuar2.xlsx';
+    const path = './assets/actuar.xlsx';
     return xlsx.parse(path);
 }
 
