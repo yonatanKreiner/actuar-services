7c70da6c549849928ed77e46c9aff2a74cf0cecc
ofirelarat <96ofir11@gmail.com>
2021-10-06 23:00:19 +0300

    add interest explain table

--- interest-calculator/src/interestCalculation.js
+++ interest-calculator/src/interestCalculation.js
@@ -5,9 +5,10 @@ const getInterestDifferences = require('./interest');
 
 const interestCalculate = async (date, debts, isLegalInterest) => {
     const allDepts = Promise.all(debts.map(async (debt) => await addExtra(date, debt, isLegalInterest)));
-    const finalDebt = (await allDepts).reduce((a, b) => a + b, 0);
+    const allDebtsResults = await allDepts;
+    const finalDebt = allDebtsResults.reduce((debtsSum, debt) => debtsSum + debt.totalDebt, 0);
 
-    return finalDebt;
+    return {total: finalDebt, allDepts: Object.values(allDebtsResults)};
 };
 
 const addExtra = async (date, debt, isLegalInterest) => {
@@ -19,7 +20,13 @@ const addExtra = async (date, debt, isLegalInterest) => {
     const hazmadaMadad = await getIndexate(debt.sum, debtDate, paymentDate);
     const hazmadaRibit = await getIndexate(interestDifference, debtDate, paymentDate);
 
-    return debt.sum + hazmadaMadad + interestDifference + hazmadaRibit;
+    return {
+        totalDebt: parseFloat((debt.sum + hazmadaMadad + interestDifference + hazmadaRibit).toFixed(2)),
+        deptDate: debt.date,
+        debtSum: debt.sum,
+        indexateSum: hazmadaMadad.toFixed(2),
+        totalInterest: (interestDifference + hazmadaRibit).toFixed(2)
+    };
 };
 
 module.exports = interestCalculate;
