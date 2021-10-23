4f30ba2e6e079b0f05a9a8a601bffb5bbe92f763
ofirelarat <96ofir11@gmail.com>
2021-10-23 23:24:40 +0300

    fix to table query

--- interest-calculator/src/interestCalculation.js
+++ interest-calculator/src/interestCalculation.js
@@ -13,18 +13,19 @@ const interestCalculate = async (debts) => {
 const addExtra = async (debt) => {
     const debtDate = moment(debt.startDate, 'DD/MM/YYYY').toDate();
     const paymentDate = moment(debt.endDate, 'DD/MM/YYYY').toDate();
+    const debtSum = parseFloat(debt.sum);
     
-    const interestDifference = getInterestDifferences(new Date(paymentDate), new Date(debtDate), debt.sum, debt.isLegalInterest);
+    const interestDifference = getInterestDifferences(new Date(paymentDate), new Date(debtDate), debtSum, debt.isLegalInterest);
 
-    const hazmadaMadad = await getIndexate(debt.sum, debtDate, paymentDate);
+    const hazmadaMadad = await getIndexate(debtSum, debtDate, paymentDate);
     const hazmadaRibit = await getIndexate(interestDifference, debtDate, paymentDate);
 
     return {
-        totalDebt: ((debt.sum + hazmadaMadad + interestDifference + hazmadaRibit).toLocaleString(undefined,{ minimumFractionDigits: 2 })),
-        deptDate: debt.startDate,
+        totalDebt: ((debtSum + hazmadaMadad + interestDifference + hazmadaRibit).toLocaleString(undefined,{ minimumFractionDigits: 2 })),
+        startDate: debt.startDate,
         isLegalInterest: debt.isLegalInterest,
         endDate: debt.endDate,
-        debtSum: debt.sum.toLocaleString(undefined,{ minimumFractionDigits: 2 }),
+        sum: debt.sum,
         indexateSum: hazmadaMadad.toLocaleString(undefined,{ minimumFractionDigits: 2 }),
         totalInterest: (interestDifference + hazmadaRibit).toLocaleString(undefined,{ minimumFractionDigits: 2 })
     };
