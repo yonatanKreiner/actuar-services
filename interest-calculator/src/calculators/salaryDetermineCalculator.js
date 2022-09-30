42a00cfd7cb0fc3e3a78be6d2100d0373e2bfc67
ofirelarat <96ofir11@gmail.com>
2022-09-30 18:43:12 +0300

    take care of dup dates

--- interest-calculator/src/calculators/salaryDetermineCalculator.js
+++ interest-calculator/src/calculators/salaryDetermineCalculator.js
@@ -3,7 +3,7 @@ const {getIndexate} = require('../tools/madad');
 const independentWorkerSalaryPrec = 0.16;
 const workerSalaryPrec = 0.125;
 
-const CalculateSalaryDetermine = async (salaries, calculationDate, isIndependendWorker) => {
+const CalculateSalaryDetermine = async (salaries, calculationDate) => {
     const salariesSumsPromise = salaries.map(async salary => {
         const totalReturn = parseFloat(salary.sumEmployee) + parseFloat(salary.sumCompany);
         let returnPrecOfSalary = totalReturn / parseFloat(salary.sum); 
@@ -16,6 +16,7 @@ const CalculateSalaryDetermine = async (salaries, calculationDate, isIndependend
         const monthlyDetermineSalary = totalReturn / returnPrecOfSalary;
 
         const indexateStartDate = new Date(salary.date);
+        indexateStartDate.setHours(0);indexateStartDate.setMinutes(0);indexateStartDate.setSeconds(0);indexateStartDate.setMilliseconds(0);
         indexateStartDate.setMonth(-2);
         const monthlyDetermineSalaryIndexate = await getIndexate(monthlyDetermineSalary, indexateStartDate, new Date(calculationDate));
      
@@ -32,25 +33,44 @@ const CalculateSalaryDetermine = async (salaries, calculationDate, isIndependend
         });
     });
 
-    const salariesSums = await Promise.all(salariesSumsPromise)
-    const lastThreeAvg = (salariesSums[salariesSums.length - 1].monthlyDetermineSalaryIndexate +
-                            salariesSums[salariesSums.length - 2].monthlyDetermineSalaryIndexate +
-                            salariesSums[salariesSums.length - 3].monthlyDetermineSalaryIndexate) / 3;
+    const salariesSums = await Promise.all(salariesSumsPromise);
+
+    const noDupSalariesCalc = []; 
+    for (let i = 0; i < salariesSums.length; i++) {
+        if (i + 1 < salariesSums.length && salariesSums[i].date === salariesSums[i + 1].date) {
+            const salaryCalc = salariesSums[i]; 
+            salaryCalc.sum += salariesSums[i + 1].sum;
+            salaryCalc.sumEmployee += salariesSums[i + 1].sumEmployee;
+            salaryCalc.sumCompany += salariesSums[i + 1].sumCompany;
+            salaryCalc.totalReturn += salariesSums[i + 1].totalReturn;
+            salaryCalc.returnPrecOfSalary += salariesSums[i + 1].returnPrecOfSalary;
+            salaryCalc.monthlyDetermineSalary += salariesSums[i + 1].monthlyDetermineSalary;
+            salaryCalc.monthlyDetermineSalaryIndexate += salariesSums[i + 1].monthlyDetermineSalaryIndexate;
+            i++;
+            noDupSalariesCalc.push(salaryCalc);
+        }else{
+            noDupSalariesCalc.push(salariesSums[i]);
+        }
+    }
+
+    const lastThreeAvg = (noDupSalariesCalc[noDupSalariesCalc.length - 1].monthlyDetermineSalaryIndexate +
+                            noDupSalariesCalc[noDupSalariesCalc.length - 2].monthlyDetermineSalaryIndexate +
+                            noDupSalariesCalc[noDupSalariesCalc.length - 3].monthlyDetermineSalaryIndexate) / 3;
 
     
     let lastTwelthSum = 0;
-    if(salariesSums.length >= 12){
+    if(noDupSalariesCalc.length >= 12){
         for(let i=1; i<=12; i++){
-            lastTwelthSum += salariesSums[salariesSums.length - i].monthlyDetermineSalaryIndexate
+            lastTwelthSum += noDupSalariesCalc[noDupSalariesCalc.length - i].monthlyDetermineSalaryIndexate
         }
     }
     const lastTwelthAvg = lastTwelthSum / 12;
 
 
     let lastFormerTwelthSum = 0;
-    if(salariesSums.length >= 24){
+    if(noDupSalariesCalc.length >= 24){
         for(let i=13; i<=24; i++){
-            lastFormerTwelthSum += salariesSums[salariesSums.length - i].monthlyDetermineSalaryIndexate
+            lastFormerTwelthSum += noDupSalariesCalc[noDupSalariesCalc.length - i].monthlyDetermineSalaryIndexate
         }
     }
     const lastFormerTwelthAvg = lastFormerTwelthSum / 12;
