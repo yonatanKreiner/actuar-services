544de2ec09a9bad9c3227359c5b0360f8b648ee6
ofirelarat <96ofir11@gmail.com>
2022-12-23 17:48:45 +0200

    try fix axios req

--- interest-calculator/src/tools/madad.js
+++ interest-calculator/src/tools/madad.js
@@ -1,5 +1,11 @@
 const axios = require('axios');
 
+const agent = new https.Agent({  
+    rejectUnauthorized: false,
+    keepAlive: true 
+});
+
+
 // The madad API doc
 // https://www.cbs.gov.il/he/Pages/%D7%9E%D7%93%D7%93%D7%99-%D7%9E%D7%97%D7%99%D7%A8%D7%99%D7%9D-%D7%91%D7%90%D7%9E%D7%A6%D7%A2%D7%95%D7%AA-API.aspx
 
@@ -11,7 +17,9 @@ const getIndexate = async (value, startDate, endDate) => {
     
     let result;
     try {
-        const res = await axios.get(`https://api.cbs.gov.il/index/data/calculator/120010?value=${value.toFixed(2)}&date=${startdateString}&toDate=${enddateString}&format=json&download=false`)
+        const res = await axios.get(
+            `https://api.cbs.gov.il/index/data/calculator/120010?value=${value.toFixed(2)}&date=${startdateString}&toDate=${enddateString}&format=json&download=false`
+            , { httpsAgent: agent})
         result = res.data.answer.to_value;
     }catch(e) {
         console.log(`failed calculate indexate ${startdateString} - ${enddateString}, with err: ${e}`);
