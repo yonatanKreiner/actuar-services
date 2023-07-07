6c585b062bbf403c61a75292d25dc7d4b7727602
ofirelarat <96ofir11@gmail.com>
2023-07-07 09:49:42 +0300

    new annuity table

--- interest-calculator/src/calculators/annuityDepositsCalculator.js
+++ interest-calculator/src/calculators/annuityDepositsCalculator.js
@@ -3,8 +3,7 @@ const annuityRepo = require('../repos/annuity-repo');
 
 const calculateAnnuities = async (deposits) => {
     const results = deposits.map(async deposit => {
-        const depositeDate = moment(deposit.paymentMonth, "YYYYMM").toDate();
-        const yearlyAnnuity = await annuityRepo.getOne(depositeDate.getFullYear());
+        const yearlyAnnuity = await annuityRepo.getOne(deposit.year);
 
         if(!yearlyAnnuity){
             return {
@@ -12,13 +11,9 @@ const calculateAnnuities = async (deposits) => {
             }
         }
 
-        const employeeMax = yearlyAnnuity.avgSalary * 0.07;
-        const companyMax = yearlyAnnuity.annuityFreeFromTax * 0.075;
-        const compensationMax = yearlyAnnuity.maxCompensation * 0.0833;
-
-        const depositeFreeEmployee = deposit.depositeEmpoloyee - employeeMax > 0 ? deposit.depositeEmpoloyee - employeeMax : 0; 
-        const depositeFreeCompany = deposit.depositeCompany - companyMax > 0 ? deposit.depositeCompany - companyMax : 0; 
-        const depositeFreeCompensation = deposit.depositeCompensation - compensationMax > 0 ? deposit.depositeCompensation - compensationMax : 0; 
+        const depositeFreeEmployee = deposit.depositeEmpoloyee - yearlyAnnuity.employeeMax > 0 ? deposit.depositeEmpoloyee - yearlyAnnuity.employeeMax : 0; 
+        const depositeFreeCompany = deposit.depositeCompany - yearlyAnnuity.companyMax > 0 ? deposit.depositeCompany - yearlyAnnuity.companyMax : 0; 
+        const depositeFreeCompensation = deposit.depositeCompensation - yearlyAnnuity.compensationMax > 0 ? deposit.depositeCompensation - yearlyAnnuity.compensationMax : 0; 
         const total = depositeFreeEmployee + depositeFreeCompany + depositeFreeCompensation;
 
         return {
